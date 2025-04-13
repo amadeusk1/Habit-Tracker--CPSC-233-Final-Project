87fb1a560ecd314bff467be5188c3a9c5de898b1
dominik.trzesicki <dominik.trzesicki@ucalgary.ca>
2025-04-12 22:33:13 -0600

    Left side layout made

--- src/main/java/ca/ucalgary/ahnaf/farhankhan/groupprojectgui1/MainController.java
+++ src/main/java/ca/ucalgary/ahnaf/farhankhan/groupprojectgui1/MainController.java
@@ -2,6 +2,10 @@ package ca.ucalgary.ahnaf.farhankhan.groupprojectgui1;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.MenuButton;
+import javafx.scene.control.TextField;
+import javafx.scene.input.ContextMenuEvent;
+import javafx.scene.layout.Pane;
 
 public class MainController {
     @FXML
@@ -11,4 +15,26 @@ public class MainController {
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
     }
+
+    @FXML
+    private Pane DayChoice;
+
+    @FXML
+    private MenuButton GoalActivityChoice;
+
+    @FXML
+    private TextField exercise;
+
+    @FXML
+    private TextField leisure;
+
+    @FXML
+    private TextField sleep;
+
+    @FXML
+    private TextField study;
+
+    @FXML
+    private TextField work;
+
 }
