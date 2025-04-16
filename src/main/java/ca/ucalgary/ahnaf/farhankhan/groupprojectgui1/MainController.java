137fe9eb2af3268ee2deb4511d764a317608b606
dominik.trzesicki <dominik.trzesicki@ucalgary.ca>
2025-04-15 23:32:21 -0600

    activity display now works

--- src/main/java/ca/ucalgary/ahnaf/farhankhan/groupprojectgui1/MainController.java
+++ src/main/java/ca/ucalgary/ahnaf/farhankhan/groupprojectgui1/MainController.java
@@ -1,139 +1,7 @@
-//package ca.ucalgary.ahnaf.farhankhan.groupprojectgui1;
-//
-//import ca.ucalgary.ahnaf.farhankhan.groupprojectgui1.objects.Activity;
-//import ca.ucalgary.ahnaf.farhankhan.groupprojectgui1.objects.Goals;
-//import javafx.collections.FXCollections;
-//import javafx.event.ActionEvent;
-//import javafx.fxml.FXML;
-//import javafx.scene.control.*;
-//import javafx.scene.input.ContextMenuEvent;
-//import javafx.scene.layout.Pane;
-//
-//public class MainController {
-//    @FXML
-//    private Label welcomeText;
-//
-//    @FXML
-//    protected void onHelloButtonClick() {
-//        welcomeText.setText("Welcome to JavaFX Application!");
-//    }
-//
-//    @FXML
-//    private ChoiceBox<String> DayChoice;
-//
-//    @FXML
-//    private ChoiceBox<String> GoalActivityChoice;
-//
-//    @FXML
-//    private Label systemUpdates;
-//
-//    @FXML
-//    private Label dayText;
-//
-//    @FXML
-//    private TextField exercise;
-//
-//    @FXML
-//    private TextField leisure;
-//
-//    @FXML
-//    private TextField sleep;
-//
-//    @FXML
-//    private TextField study;
-//
-//    @FXML
-//    private TextField work;
-//
-//    @FXML
-//    private Button confirm;
-//
-//    @FXML
-//    private TextArea GoalsDisplay;
-//
-//    @FXML
-//    public void initialize() {
-//        // Populate the DayChoice menu
-//        DayChoice.setItems(FXCollections.observableArrayList("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"));
-//
-//        // Populate the Goal / activity menu
-//        GoalActivityChoice.setItems(FXCollections.observableArrayList("Goals", "Activity"));
-//
-//        // Set DayChoice to be hidden by default since initial selection is "Goal"
-//        DayChoice.setVisible(false);
-//        dayText.setVisible(false);
-//
-//
-//        // Listen for changes on GoalActivityChoice
-//        GoalActivityChoice.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
-//            // Show the DayChoice only if the user selects "Habit"
-//            if ("Activity".equals(newValue)) {
-//                DayChoice.setVisible(true);
-//                dayText.setVisible(true);
-//            } else {
-//                DayChoice.setVisible(false);
-//                dayText.setVisible(false);
-//            }
-//        });
-//    }
-//
-//
-//    @FXML
-//    void confirmAction(ActionEvent event) {
-//        // Get the selected values from goals
-//        String selectedGoal = GoalActivityChoice.getValue();
-//        try {
-//            int sleepV = Integer.parseInt(sleep.getText());
-//            int exerciseV = Integer.parseInt(exercise.getText());
-//            int studyV = Integer.parseInt(study.getText());
-//            int workV = Integer.parseInt(work.getText());
-//            int leisureV = Integer.parseInt(leisure.getText());
-//            // check if inputs are positive
-//            if (sleepV < 0 || exerciseV < 0 || studyV < 0 || workV < 0 || leisureV < 0) {
-//                systemUpdates.setText("All inputs must be positive numbers.");
-//                return;
-//            }
-//            // sum total inputs
-//            int sumTotal = sleepV + exerciseV + studyV + workV + leisureV;
-//
-//            // if goals is selected
-//            if ("Goals".equals(selectedGoal)) {
-//                if (sumTotal <= 24) {
-//                    Goals goals = new Goals(sleepV, exerciseV, studyV, workV, leisureV);
-//                    GoalsDisplay.setText(goals.toString());
-//                    systemUpdates.setText("Goals logged successfully");
-//                } else {
-//                    systemUpdates.setText("The total of your goals must be less than or equal to 24 hours. Please try again.");
-//                }
-//            } else if ("Activity".equals(selectedGoal)) {
-//                String selectedDay = DayChoice.getValue();
-//                if (sumTotal <= 24) {
-//                    Activity activity = new Activity(selectedDay, sleepV, exerciseV, studyV, workV, leisureV);
-//                    systemUpdates.setText("Activities logged successfully");
-//                } else {
-//                    systemUpdates.setText("The total of your goals must be less than or equal to 24 hours. Please try again.");
-//                }
-//
-//            } else {
-//                systemUpdates.setText("Select Goal / Activity");
-//            }
-//
-//
-//        } catch (NumberFormatException e) {
-//            systemUpdates.setText("Invalid input. Please enter a valid integer for all inputs .");
-//            // Optionally, display an error message to the user, e.g., using an Alert.
-//        }
-//
-//    }
-//
-//}
-
-
-
-
 package ca.ucalgary.ahnaf.farhankhan.groupprojectgui1;
 
 import ca.ucalgary.ahnaf.farhankhan.groupprojectgui1.objects.Activity;
+import ca.ucalgary.ahnaf.farhankhan.groupprojectgui1.objects.Day;
 import ca.ucalgary.ahnaf.farhankhan.groupprojectgui1.objects.Goals;
 import ca.ucalgary.ahnaf.farhankhan.groupprojectgui1.util.FileLoader;
 import ca.ucalgary.ahnaf.farhankhan.groupprojectgui1.util.FileSaver;
@@ -151,9 +19,12 @@ import javafx.stage.Stage;
 
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
 import javafx.scene.paint.Color;
 
-import static ca.ucalgary.ahnaf.farhankhan.groupprojectgui1.Data.goal;
+import static ca.ucalgary.ahnaf.farhankhan.groupprojectgui1.Data.*;
 
 
 public class MainController {
@@ -225,15 +96,15 @@ public class MainController {
         fc.setInitialFileName("data.csv");
         File file = fc.showOpenDialog(new Stage());
         load(file);
-        menuViewAllActivities();
-//        menuViewAllGoals();
+        ActivityDisplay.setText(Data.displayAllActivitiesGUI());
+//       menuViewAllGoals();
+//        menuViewAllActivities();
         GoalsDisplay.setText(goal.toString());
     }
 
-    public static void menuViewAllActivities() {
-//        Data.displayAllActivities();
-
-    }
+//    public void menuViewAllActivities() {
+//        Data.displayAllActivitiesGUI();
+//    }
 
 //    public static void menuViewAllGoals() {
 //        System.out.println("\nCurrent Daily Goals:");
@@ -335,7 +206,8 @@ public class MainController {
                 String selectedDay = DayChoice.getValue();
                 if (sumTotal <= 24) {
                     Activity activity = new Activity(selectedDay, sleepV, exerciseV, studyV, workV, leisureV);
-                    ActivityDisplay.appendText(activity.toString());
+                    storeNewDay(selectedDay, sleepV, exerciseV, studyV, workV, leisureV);
+                    ActivityDisplay.setText(Data.displayAllActivitiesGUI());
                     status_label.setText("Activities logged successfully");
                 } else {
                     status_label.setText("The total of your goals must be less than or equal to 24 hours. Please try again.");
@@ -348,7 +220,6 @@ public class MainController {
 
         } catch (NumberFormatException e) {
             status_label.setText("Invalid input. Please enter a valid integer for all inputs .");
-            // Optionally, display an error message to the user, e.g., using an Alert.
         }
 
     }
