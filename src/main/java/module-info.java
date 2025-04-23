3695f948c69f515f4420b0056252f81fcaf633bd
AhnafFarhanKhan <ahnaffarhankhan@gmail.com>
2025-04-22 18:52:22 -0600

    .

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,8 +2,6 @@ module ca.ucalgary.ahnaf.farhankhan.groupprojectgui1 {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.desktop;
-    requires org.testng;
-    requires org.junit.jupiter.api;
 
 
     opens ca.ucalgary.ahnaf.farhankhan.groupprojectgui1 to javafx.fxml;
